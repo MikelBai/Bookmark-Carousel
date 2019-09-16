0860bc213b2b96fe2849cbcdca6b6cdaf36133bf
leoli <loe.li@mail.utoronto.ca>
2019-09-16 00:44:40 -0400

    Basic carousel added, kinda losing the purpose of the additional features due to innate utility of fchrome.

--- js/background.js
+++ js/background.js
@@ -1,21 +1,51 @@
+// Showing page action if it's a bookmark
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({color: '#3aa757'}, function() {
         console.log("The color is green but that's not the point of this extension.");
     });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function(){
-        chrome.declarativeContent.onPageChanged.addRules([onBookmarkedRule, defaultRule]);
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                isBookmarked: true,
+            })],
+            
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
     });
 });
-var onBookmarkedRule = {
-    conditions: [new chrome.declarativeContent.PageStateMatcher({
-        isBookmarked: true,
-    })],
-    
-    actions: [chrome.browserAction.setPopup({popup: "on_bookmarked.html"})]}
-var defaultRule = {
-    conditions: [new chrome.declarativeContent.PageStateMatcher({
-        isBookmarked: false,
-    })],
-    
-    actions: [chrome.browserAction.setPopup({popup: "default_popup.html"})]
+
+// Giving that random bookmark
+chrome.commands.onCommand.addListener(function(){
+    chrome.bookmarks.getTree(function(bookmarks) {
+        var carousel = [];
+        bookmarks.forEach(function(treenode) {
+            carousel = carousel.concat(processNode(treenode));
+        });
+        var rand_bm = carousel[Math.floor(Math.random() * carousel.length)];
+        chrome.tabs.create({
+            url: rand_bm
+        });
+    });
+});
+
+function processNode(node) {
+    // base case: has a url (aka not a folder)
+    if (node.url) {
+        return [node.url];
+    }
+    // recursive step: has children
+    var urls = [];
+    if (node.children) {
+        node.children.forEach(function(childNode) {
+            urls = urls.concat(processNode(childNode));
+        });
+        return urls;
+    }
 }
+
+document.getElementById("preserve").onClick = function() {letOffCarousel()};
+
+// function letOffCarousel() {
+//     if ()
+// }
+
