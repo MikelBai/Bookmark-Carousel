f6ab87bf794b83a6d4a6a480c7745c94427e83c7
leoli <loe.li@mail.utoronto.ca>
2019-09-15 20:06:17 -0400

    Halfway towards basic functionality -- outlined bookmark popup behaviour and which folders particular bookmarks might go towards

--- js/background.js
+++ js/background.js
@@ -3,11 +3,19 @@ chrome.runtime.onInstalled.addListener(function() {
         console.log("The color is green but that's not the point of this extension.");
     });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function(){
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                isBookmarked: true,
-            })],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
+        chrome.declarativeContent.onPageChanged.addRules([onBookmarkedRule, defaultRule]);
     });
 });
+var onBookmarkedRule = {
+    conditions: [new chrome.declarativeContent.PageStateMatcher({
+        isBookmarked: true,
+    })],
+    
+    actions: [chrome.browserAction.setPopup({popup: "on_bookmarked.html"})]}
+var defaultRule = {
+    conditions: [new chrome.declarativeContent.PageStateMatcher({
+        isBookmarked: false,
+    })],
+    
+    actions: [chrome.browserAction.setPopup({popup: "default_popup.html"})]
+}
